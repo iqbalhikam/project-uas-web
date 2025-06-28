84de6d58a841893c9a3b4124fccfcd59e55d62bd
iqbal hikam <idiscrus@gmail.com>
2025-06-28 11:07:22 +0700

    tahap 4

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,29 @@
-// src/middleware.ts
 
-// Baris ini mendelegasikan semua pekerjaan ke NextAuth
-export { default } from 'next-auth/middleware';
+import { withAuth } from 'next-auth/middleware';
+import { NextResponse } from 'next/server';
 
-// Baris ini tetap sama, untuk menentukan rute yang dilindungi
+export default withAuth(
+  // `withAuth` akan memperkaya `req` dengan token pengguna.
+  function middleware(req) {
+    const { token } = req.nextauth;
+
+    // Cek jika pengguna mencoba mengakses rute admin (/dashboard/users)
+    if (req.nextUrl.pathname.startsWith('/dashboard/users')) {
+      // Jika role bukan ADMIN, alihkan ke halaman utama dashboard
+      if (token?.role !== 'ADMIN') {
+        return NextResponse.redirect(new URL('/dashboard', req.url));
+      }
+    }
+  },
+  {
+    callbacks: {
+      // Callback ini untuk menentukan apakah pengguna diizinkan mengakses (authorized)
+      authorized: ({ token }) => !!token, // Pengguna dianggap ter-otorisasi jika ada token (sudah login)
+    },
+  }
+);
+
+// Tentukan rute yang ingin dilindungi oleh middleware ini
 export const config = {
   matcher: ['/dashboard/:path*'],
 };
