cc0214702f7b6d16a3bc745959fc01ef53e39818
iqbal hikam <idiscrus@gmail.com>
2025-07-02 22:24:31 +0700

    Fixed Type Error on Goods Receipt Page 1

--- src/app/dashboard/purchases/receive/[id]/page.tsx
+++ src/app/dashboard/purchases/receive/[id]/page.tsx
@@ -3,10 +3,13 @@
 import { getPurchaseOrderById } from '@/lib/actions/purchase.actions';
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card';
 import { GoodsReceiptForm } from '@/components/purchases/GoodsReceiptForm';
+// import { id } from 'date-fns/locale';
 
-export default async function GoodsReceiptPage({ params }: { params: { id: string } }) {
-  const { id } = params;
-  const { purchaseOrder, error } = await getPurchaseOrderById(id);
+type Params = Promise<{slug :  string}>;
+
+export default async function GoodsReceiptPage(props: { params: Params}) {
+    const params = await props.params;
+  const { purchaseOrder, error } = await getPurchaseOrderById(params.slug);
 
   if (error || !purchaseOrder) {
     return <div className="p-8 text-center text-red-500">{error || 'Order pembelian tidak ditemukan.'}</div>;
